25c8d5ecc61b9627f1435cd571879dc4668cb318
shreya pandey <32763445+shreyapy@users.noreply.github.com>
2018-01-30 21:43:49 +0530

    Create turtlegameREADME.py

---
+++ turtlegameREADME.py
@@ -0,0 +1,11 @@
+# turtle
+this is a simple user interactive program which draws beautiful projects nd shapes that u might like.
+import turtle 
+my_turtle=turtle.Turtle()
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
+my_turtle.left(90)
+my_turtle.forward(100)
